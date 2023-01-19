56d6dadad226dbe56e07959f8c566bfa831e4815
jerico-esguerra <jericoesguerra212@outlook>
2023-01-19 09:36:08 -0500

    fix deployment bug

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const Home = () => {
   return (
     <>
       HeroBanner
@@ -25,3 +20,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Home
